6ceb76a0a8fd6c6ff80bb6c4a1b4db2130f74fce
Matt Orvin <matthew.orvin@gmail.com>
2019-04-29 19:27:03 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -57,7 +57,7 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.collect do |season, data|
     data.collect do |holiday, supply|
      supply.select{|item| item == "BBQ"}
-      holiday.select.supply
+      holiday.supply.select
   end
 end.flatten
 end
